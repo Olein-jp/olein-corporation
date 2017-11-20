0f860875ec1d252eec779e0500f3c75026be50fd
KojiKuno <koji.kuno@gmail.com>
2017-11-20 15:05:28 +0900

    building

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html lang="ja">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="/assets/css/style.css">
-	</head>
-	<body>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+	<body <?php body_class(); ?>>
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'oleincorp' ); ?></a>
+			<header id="masthead" class="site-header">
+
+			</header>
+			<div id="content" class="site-content">
+
+			</div>
+			<footer id="colophone" class="site-footer">
+				
+			</footer>
+		</div>
+		<?php wp_footer(); ?>
 	</body>
 </html>
